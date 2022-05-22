044610d404ab22ba83db0ff4e161b836a8a80b62
Alexander Kirillov <103785761+realworie@users.noreply.github.com>
2022-05-22 20:47:34 +0300

    Update task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -1,6 +1,12 @@
 import math
 import sys
 
+def count_steps(number_array, target_level):
+    c = 0
+    for number in number_array:
+        c += abs(target_level - number)
+    return c
+
 filename = sys.argv[1]
 
 nums = []
@@ -13,20 +19,4 @@ file.close()
 average_round_down = math.floor(sum(nums) / len(nums))
 average_round_up = average_round_down + 1
 
-length = len(nums)
-counter_c = 0
-counter_f = 0
-
-for number in nums:
-    if number < average_round_down:
-        counter_f += average_round_down - number
-        counter_c += average_round_down - number + 1
-    elif number > average_round_up:
-        counter_c += number - average_round_up
-        counter_f += number - average_round_up + 1
-    elif number == average_round_down:
-        counter_c += 1
-    elif number == average_round_up:
-        counter_f += 1
-
-print(str(min(counter_c, counter_f)))
+print(str(min(count_steps(nums, average_round_down), count_steps(nums, average_round_up))))
